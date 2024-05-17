491da152c577cd92d26d0bb85cdea2b8c0737d26
abhishek <rathi.abhishek3007@gmail.com>
2024-05-17 15:22:59 +0530

    basic UI added

---
+++ todo-list/src/components/todoList/index.js
@@ -0,0 +1,18 @@
+function TodoList () {
+
+    return (
+      <div>
+        <form>
+          <input type="text" />
+          <button>Add Todo</button>
+        </form>
+        <ul>
+            <li>
+           </li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default TodoList;
+  
