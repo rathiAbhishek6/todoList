75d22c02f3e11f447501eabf451ba9cd05e30717
Abhishek <rathi.abhishek3007@gmail.com>
2024-05-17 18:12:12 +0530

    local storage & style added

--- todo-list/src/components/todoList/index.js
+++ todo-list/src/components/todoList/index.js
@@ -1,45 +1,65 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import './index.css'
 
-function TodoList () {
+function TodoList() {
   // intializing the state
-  const [todos, setTodos] = useState([])
-  const [inputValue, setInputValue] = useState('')
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-function handleChange(e){
-  //it will update the inputValue when the user changes the input
-  setInputValue(e.target.value)
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
+
+  // Save todos to local storage whenever they are updated
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  function handleChange(e) {
+    //it will update the inputValue when the user changes the input
+    setInputValue(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (inputValue.trim() !== '') {
+    // it will copy the array and adds input value at last of the array
+    setTodos([...todos, inputValue]);
+    // when user clicks on submit button, it will clear the input box
+    setInputValue("");
+  }
 }
 
-function handleSubmit(e){
-  e.preventDefault()
- // it will copy the array and adds input value at last of the array
-  setTodos([...todos, inputValue])
-  // when user clicks on submit button, it will clear the input box
-  setInputValue('')
-}
-
-function handleDelete(index) {
-  //  filter to create a new array excluding the todo at the specified index
-  const updatedTodos = todos.filter((_, i) => i !== index);
-  // Update the todos state with the new array
-  setTodos(updatedTodos);
-}
+  function handleDelete(index) {
+    //  filter to create a new array excluding the todo at the specified index
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    // Update the todos state with the new array
+    setTodos(updatedTodos);
+  }
   return (
     <div>
       <h1>Todo List</h1>
       <form>
-        <input type='text' value={inputValue} onChange={handleChange}/>
+        <input type="text" value={inputValue} onChange={handleChange} />
         <button onClick={handleSubmit}>Add Todo</button>
       </form>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>{todo}
-          <button onClick={() =>handleDelete(index)}>Delete</button>
-          </li>
+          <div className="flexItem">
+            {" "}
+            <li key={index}>
+              {todo}
+              </li>
+              <button className='btn' onClick={() => handleDelete(index)}>Delete</button>
+           
+          </div>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default TodoList;
